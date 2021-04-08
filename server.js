82a9b58917169262e26711e59a4afe8994fb8f89
Brian Kim <brian.kyounghoon.kim@gmail.com>
2021-04-08 17:00:20 +0900

    post link add link update

--- server.js
+++ server.js
@@ -7,10 +7,24 @@ const cors = require("cors");
 
 app.use(cors());
 
+app.use(express.json());
+
 app.use("/uploads", express.static("uploads"));
 
 app.get("/post", (req, res) => {
   res.status(200).send(postData.get());
+});
+
+app.post("/newPost", (req, res) => {
+  const newPost = {
+    "id": `${Date.now()}`,
+    "title": req.body.title,
+    "content": req.body.content,
+    "post_image": req.body["post_image"],
+    "added_date": `${Date.now()}`,
+  };
+  postData.add(newPost);
+  res.status(200).send(newPost);
 });
 
 app.get("/post/:post_id", (req, res) => {
